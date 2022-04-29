493359a2543d2edee25427d9d7ddd4ddc4e97ce7
Bernardo <djalmirinha@gmail.com>
2022-04-29 13:41:32 -0300

    Add basic pytest config

---
+++ api/tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+
+from neoway.app_factory import create_app
+from neoway.extensions import db
+from neoway.models import CPFModel, CNPJModel
+
+@pytest.fixture(scope='module')
+def app():
+    """Instância da aplicação principal do Flask"""
+    app = create_app()
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/dev.db'
+
+    with app.app_context():
+        db.create_all()
+        populate_db(db)
+        yield app
+
+        db.drop_all()
+
+def populate_db(db):
+    db.session.add(CPFModel(cpf='11421433923'))
+    db.session.add(CPFModel(cpf='44826646186'))
+
+    db.session.add(CNPJModel(cnpj='46911271000108'))
+    db.session.add(CNPJModel(cnpj='13617419000139'))
+
+    db.session.commit()
